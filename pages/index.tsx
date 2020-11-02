a41fd4c7e96af51066623acc36ce6cfd7ac07513
Cezar Neaga <hi@cezarneaga.eu>
2020-11-02 22:52:53 +0100

    convert to typescript and clean app

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p>
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <footer>
+        <a
+          href="https://cezarneaga.eu"
+          target="_blank"
+          rel="noopener noreferrer">
+          Powered by next
+        </a>
+      </footer>
+    </div>
+  )
+}

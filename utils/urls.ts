9f40f0ed2d09f488692e1bf5055121a940f4b3b8
Cezar Neaga <hi@cezarneaga.eu>
2020-11-06 11:23:28 +0100

    add social media links

---
+++ utils/urls.ts
@@ -0,0 +1,9 @@
+export const githubUrl = 'https://github.com/cezarneaga'
+
+export const twitterUrl = 'https://twitter.com/cezarneaga'
+
+export const facebookUrl = 'https://www.facebook.com/cezar.neaga'
+
+export const linkedinUrl = 'https://www.linkedin.com/in/cezarneaga'
+
+export const constnextUrl = 'https://constnext.com'

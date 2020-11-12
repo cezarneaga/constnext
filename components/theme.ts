3d5b6066105ff58cdb7ac01c84b5514a4a296dac
Cezar Neaga <hi@cezarneaga.eu>
2020-11-12 10:48:30 +0100

    add breakpoints object and some colors

--- components/theme.ts
+++ components/theme.ts
@@ -1,13 +1,32 @@
 import { extendTheme } from '@chakra-ui/core'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+
+// Then add your custom breakpoints as key-value pairs
+const breakpoints = createBreakpoints({
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+})
 const colors = {
   brand: {
     900: '#1a365d',
     800: '#153e75',
-    700: '#2a69ac',
+    700: '#2b5d93',
+    400: '#4278b3',
+    300: '#699acf',
   },
   gray: {
     900: '#353740',
+    800: '#212933',
+  },
+  transparent: {
+    black: 'rgba(33, 41, 51, 0.76)',
+    white: 'rgba(255,255,255,0.76)',
   },
 }
-
-export const customTheme = extendTheme({ colors })
+const fonts = {
+  body: '"Open Sans", Arial, system-ui, sans-serif',
+  heading: '"Montserrat", Helvetica, Arial, Lucida, sans-serif',
+}
+export const customTheme = extendTheme({ colors, fonts, breakpoints })

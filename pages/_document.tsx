4de39fcd86070c0d0bca41e6fcd9fbd33037eb4f
Cezar Neaga <hi@cezarneaga.eu>
2020-11-22 08:25:22 +0100

    add splitbee and remove gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,21 +17,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script async data-api="/_hive" src="/bee.js"></script>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
           <link rel="icon" href="/images/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link

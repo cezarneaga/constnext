f93cd2f327404f014c0226d5af320d5646d66d87
Cezar Neaga <hi@cezarneaga.eu>
2020-11-22 16:29:54 +0100

    remove forgotten setup for ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,9 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { DefaultSeo } from 'next-seo'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import SEO from '../next-seo.config'
-import * as gtag from '../lib/gtag'
 import { customTheme } from 'theme/theme'
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
   return (
     <ChakraProvider theme={customTheme}>
       <DefaultSeo {...SEO} />
